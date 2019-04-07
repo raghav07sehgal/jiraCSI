a0068e97dcfd785c32e99f2607258da186a916b6
raghav <raghavsehgal129@gmail.com>
2019-04-08 05:26:47 +0530

    final update

--- jiraSearch.js
+++ jiraSearch.js
@@ -2,13 +2,14 @@ var search = require('jira-search');
 var localStorage = require('localStorage');
 var fromDate, assignee, historyData, toDate, totalDays, IRformatTime, IPformatTime, OAformatTime, fromTime, toTime, nextAsTime;
 var flag = false;
-var index, historyDataIndex, historyDataLIndex;
+var index, historyDataIndex, historyDataLIndex, ticketStatus;
 var sIndex = null;
 var eIndex = null;
 var ignoreUser = ["Paras Anand", "Ritesh Aswal", "Neeraj Pant", "Karishma Jain", "HMH Release Management", "T3 QA Lead", "Release Engineering NIIT", "Prashant Dubey", "Amrita Padavil", "Rohit Kanwar", "Tier 2 - Lead", "Prashant Gupta", "Neeharika Mittal", "Dheeraj Kumar"];
 var irDate = [];
 var ipDate = [];
 var otAssDate = [];
+var OAformatTime = [];
 var dayFlag = false;
 
 search(
@@ -16,7 +17,7 @@ search(
     serverRoot: 'https://jira.hmhco.com', // the base URL for the JIRA server
     user: 'pantn', // the user name
     pass: 'Neeraj@123', // the password
-    jql: 'project = "CSI" AND status was "Inputs Required" AND assignee was in (aswalr) and createdDate >= "2018-3-27" AND createdDate< "2018-3-31"', // the JQL
+    jql: 'project = "CSI" AND status was "Inputs Required" AND assignee was in (aswalr) and createdDate >= "2019-3-27" AND createdDate< "2019-3-31"', // the JQL
     fields: '*all', // the fields parameter for the JIRA search
     expand: 'changelog', // the expand parameter for the JIRA search
     maxResults: 10, // the maximum number of results for each request to JIRA, multiple requests will be made till all the matching issues have been collected
@@ -26,18 +27,23 @@ search(
     mapCallback: function (issue) {
 
       if (issue) {
-        console.log(issue.key + "\n");
+        console.log(issue.key);
+        ticketStatus = issue.fields.status.name;
+        console.log("Current Ticket Status - " + ticketStatus + "\n");
+
         historyData = issue.changelog.histories;
         irDate = [];
         ipDate = [];
         otAssDate = [];
-        fromTime = "", toTime = "", nextAsTime = "";
+        OAformatTime = [];
+        fromTime = "", toTime = "", nextAsTime = [];
         for (let i = 0; i < historyData.length; i++) {
           let date = historyData[i].created;
           let items = historyData[i].items;
           index = null;
           sIndex = null;
           eIndex = null;
+          dayFlag = true;
           for (let j = 0; j < items.length; j++) {
             let statusField = items[j].field;
             let fieldType = items[j].fieldtype;
@@ -69,7 +75,8 @@ search(
                   toDate = formatDate.getFullYear() + "-" + (formatDate.getMonth() + 1) + '-' + formatDate.getDate();
                   let printDate = formatDate.getDate() + "/" + (formatDate.getMonth() + 1) + "/" + formatDate.getFullYear();
                   ipDate.push(toDate);
-                  console.log("Return to NIIT Date - " + printDate);
+                  console.log("Return to NIIT Date - " + printDate + "\n");
+
                   let time = returnDate[1];
                   let formatTime = time.split(".");
                   IPformatTime = formatTime[0];
@@ -81,7 +88,7 @@ search(
               }
               if (index != null && statusField == "assignee") {
                 assignee = items[index + 1].toString;
-                console.log("IR assignee name - " + assignee + "\n");
+                console.log("IR assignee name - " + assignee);
               }
 
             }
@@ -89,7 +96,6 @@ search(
           if (sIndex) {
             historyDataIndex = i;
             localStorage.setItem("stIndex", historyDataIndex);
-
             otherAssignee(historyDataIndex, null);
           }
           if (eIndex) {
@@ -123,9 +129,8 @@ search(
                       otAssDate.push(fromDate);
                       let time = moveDate[1];
                       let formatTime = time.split(".");
-                      OAformatTime = formatTime[0];
+                      OAformatTime.push(formatTime[0]);
                       calculateDays(irDate, ipDate, otAssDate, IRformatTime, IPformatTime, OAformatTime);
-
                     }
                     else {
                       continue;
@@ -150,12 +155,25 @@ search(
             if (nextAsDate.length > 1) {
               var fstAsDate = nextAsDate[0];
               for (let i = 1; i < nextAsDate.length; i++) {
-                debugger
-                var startDate = Date.parse(fstAsDate);
-                var endDate = Date.parse(nextAsDate[i]);
-                var timeDiff = startDate - endDate;
-                daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-                console.log("Assignee between other assignee difference days - " + daysDiff + "\n");
+                if (fstAsDate == nextAsDate[i]) {
+                  let splitDate1 = fstAsDate.split('-');
+                  let splitDate2 = nextAsDate[i].split('-');
+                  var time_start = new Date(splitDate1[0], splitDate1[1], splitDate1[2]);
+                  var time_end = new Date(splitDate2[0], splitDate2[1], splitDate2[2]);
+                  var value_start = nextAsTime[i].split(':');
+                  var value_end = nextAsTime[0].split(':');
+                  time_start.setHours(value_start[0], value_start[1], value_start[2], 0)
+                  time_end.setHours(value_end[0], value_end[1], value_end[2], 0)
+                  var timeInMs = time_start - time_end;
+                  var time = msToTime(timeInMs);
+                  console.log("Assignee between other assignee difference in time - " + time + "\n");
+                } else {
+                  var startDate = Date.parse(fstAsDate);
+                  var endDate = Date.parse(nextAsDate[i]);
+                  var timeDiff = startDate - endDate;
+                  daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+                  console.log("Assignee between other assignee difference days - " + daysDiff + "\n");
+                }
               }
             } else {
               for (let i = 0; i < fromDate.length; i++) {
@@ -164,7 +182,7 @@ search(
                   let splitDate2 = fromDate[i].split('-');
                   var time_start = new Date(splitDate1[0], splitDate1[1], splitDate1[2]);
                   var time_end = new Date(splitDate2[0], splitDate2[1], splitDate2[2]);
-                  var value_start = nextAsTime.split(':');
+                  var value_start = nextAsTime[i].split(':');
                   var value_end = fromTime.split(':');
                   time_start.setHours(value_start[0], value_start[1], value_start[2], 0)
                   time_end.setHours(value_end[0], value_end[1], value_end[2], 0)
