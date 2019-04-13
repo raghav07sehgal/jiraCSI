dbf24d1622e11bd8bdb9a1b57ee1688bf017e108
raghav <raghavsehgal129@gmail.com>
2019-04-13 15:06:20 +0530

    updated js from home

--- calculateDays.js
+++ calculateDays.js
@@ -12,7 +12,10 @@ var otAssDate = [];
 var OAformatTime = [];
 var dayFlag = false;
 var aSFlag = false;
-var filePath = "D:/raghavData/PROJECTS/project SM/Server/Report.xlsx";
+var returnFlag = false;
+var otFlag = false;
+// var filePath = "D:/raghavData/PROJECTS/project SM/Server/Report.xlsx";
+var filePath = "R:/my files/niit/jiraApi/jiraCSI/Report.xlsx";
 var Excel = require('exceljs');// load exceljs module
 var workbook = new Excel.Workbook(); //create object of workbook
 var newSheet = workbook.addWorksheet('TestData');//add sheet to workbook
@@ -21,12 +24,16 @@ var rowData1 = [];
 var rowData2 = [];
 var rowData3 = [];
 var rowData4 = [];
+var rowData5 = [];
 var rowiPData1 = [];
 var rowiRData1 = [];
 var iRData = [];
 var iPrData = [];
 var othAssData = [];
 var daysData = [];
+var otherAsDataAdd = [];
+
+
 
 addRowToNewExcel();
 function addRowToNewExcel() {
@@ -67,18 +74,22 @@ exports.setData = function (data) {
 					let toStatus = items[j].toString;
 					//status moved to input required
 					if (fromStatus == "Investigation & Research" && toStatus == "Inputs Required") {
+
 						index = j;
 						sIndex = j;
 						let moveDate = date.split("T");
 						let formatDate = new Date(moveDate[0]);
 						fromDate = formatDate.getFullYear() + "-" + (formatDate.getMonth() + 1) + '-' + formatDate.getDate();
 						let printDate = formatDate.getDate() + "/" + (formatDate.getMonth() + 1) + "/" + formatDate.getFullYear();
+
 						irDate.push(fromDate);
 						console.log("Move input date - " + printDate);
 						let time = moveDate[1];
 						let formatTime = time.split(".");
 						IRformatTime = formatTime[0];
+
 						calculateDays(irDate, ipDate, otAssDate, IRformatTime, IPformatTime, OAformatTime);
+
 						// iRData.push({ csi: issue.key, toName: assignee, fromDate: fromDate, daysDiff: daysDiff, tillTotalDays: SdaysDiff });
 					}
 					//status returned from input required
@@ -87,6 +98,7 @@ exports.setData = function (data) {
 						let returnDate = date.split("T");
 						let formatDate = new Date(returnDate[0]);
 						toDate = formatDate.getFullYear() + "-" + (formatDate.getMonth() + 1) + '-' + formatDate.getDate();
+						returnFlag = true;
 						let printDate = formatDate.getDate() + "/" + (formatDate.getMonth() + 1) + "/" + formatDate.getFullYear();
 						ipDate.push(toDate);
 						console.log("Return to NIIT Date - " + printDate + "\n");
@@ -100,30 +112,29 @@ exports.setData = function (data) {
 						otAssDate = [];
 					}
 				}
-				if (index != null && statusField == "assignee") {
-					assignee = items[index + 1].toString;
-					if (fromDate) {
-						iRData.push({ csi: issue.key, toName: assignee, fromDate: fromDate, daysDiff: daysDiff, tillTotalDays: SdaysDiff });
+
+				if (statusField == "assignee") {
+					//By whom assignee back to I&R state
+					// if (returnFlag && eIndex != null) {
+					// 	assignee = items[eIndex + 1].fromString;
+					// 	console.log("IR assignee name - " + assignee);
+					// 	if (toDate) {
+					// 		iPrData.push({ csi: issue.key, toName: assignee, toDate: toDate, AssdaysDiff: daysDiff, totalDays: TdaysDiff, tillTotalDays: SdaysDiff });
+					// 	}
+					// }
+					if (index != null) {
+						assignee = items[index + 1].toString;
+						console.log("IR assignee name - " + assignee);
+						if (fromDate) {
+							iRData.push({ csi: issue.key, toName: assignee, fromDate: fromDate, daysDiff: daysDiff, tillTotalDays: SdaysDiff });
+						}
 					}
-					// else if (toDate) {
-					// 	iPrData.push({ csi: issue.key, toName: assignee, toDate: toDate, AssdaysDiff: daysDiff, totalDays: TdaysDiff, tillTotalDays: SdaysDiff });
-					// 	
-					// }
-					// else{
-					// 	
-					// }
-					console.log("IR assignee name - " + assignee);
 				}
-				// 
-				// if (index != null && statusField == "status") {
-				// 	if (toDate) {
-				// 		iPrData.push({ csi: issue.key, toName: assignee, toDate: toDate, AssdaysDiff: daysDiff, totalDays: TdaysDiff, tillTotalDays: SdaysDiff });
-				// 
-				// 	}
-				// }
 			}
 		}
+
 		if (sIndex && !eIndex) {
+
 			historyDataIndex = i;
 			localStorage.setItem("stIndex", historyDataIndex);
 			otherAssignee(historyDataIndex, null);
@@ -134,7 +145,6 @@ exports.setData = function (data) {
 			var stIndex = localStorage.getItem("stIndex");
 			stIndex = parseInt(stIndex);
 			otherAssignee(stIndex, historyDataLIndex);
-			// writeExcel();
 		}
 	}
 
@@ -156,7 +166,6 @@ exports.setData = function (data) {
 								let moveDate = date.split("T");
 								let formatDate = new Date(moveDate[0]);
 								otherDate = formatDate.getFullYear() + "-" + (formatDate.getMonth() + 1) + '-' + formatDate.getDate();
-								// if (otherDate <= toDate) {
 								let printDate = formatDate.getDate() + "/" + (formatDate.getMonth() + 1) + "/" + formatDate.getFullYear();
 								console.log("Next assignee date - " + printDate);
 								console.log("Next assignee - " + checkAsssignee + "\n");
@@ -173,7 +182,12 @@ exports.setData = function (data) {
 								continue;
 							}
 						} else if (statusField == "status") {
-							break;
+							let fromStatus = items[j].fromString;
+							let toStatus = items[j].toString;
+							if (fromStatus == "Inputs Required" && toStatus == "Investigation & Research") {
+								// break;
+								return;
+							}
 						}
 					}
 				}
@@ -188,20 +202,20 @@ exports.setData = function (data) {
 		//Till next assignee day
 		if ((fromDate.length != 0) && (nextAsDate.length != 0) && (toDate.length == 0)) {
 			dayFlag = true;
+
 			//difference days between two assignees
 			if (nextAsDate.length > 1) {
 				var fstAsDate = nextAsDate[0];
 				for (let i = 1; i < nextAsDate.length; i++) {
-
 					var startDate = Date.parse(fstAsDate);
 					var endDate = Date.parse(nextAsDate[i]);
 					var timeDiff = endDate - startDate;
 					daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 					console.log("Assignee between other assignee difference days - " + daysDiff + "\n");
+					nextAsDate.shift();
 				}
 			} else {
 				for (let i = 0; i < fromDate.length; i++) {
-
 					var startDate = Date.parse(nextAsDate[i]);
 					var endDate = Date.parse(fromDate[i]);
 					var timeDiff = startDate - endDate;
@@ -240,11 +254,9 @@ exports.setData = function (data) {
 	}
 
 	writeExcel();
-
 	var checkFunction = createExcel(filePath);
 	if (checkFunction == "flagExcel") {
 		iRData = [];
-
 		iPrData = [];
 		othAssData = [];
 	}
@@ -263,17 +275,34 @@ function createExcel(excelFilePath) {
 
 function writeExcel() {
 	for (let i = 0; i < iRData.length; i++) {
-
-
 		if (iRData.length > 0 && othAssData.length > 0) {
 			for (let k = 0; k < othAssData.length; k++) {
 				if (iRData[i].csi == othAssData[k].csi) {
-					rowData1[1] = iRData[i].csi;
-					rowData1[2] = iRData[i].fromDate;
-					rowData1[4] = iRData[i].toName;
-					rowData1[3] = othAssData[k].otherDate;
-					rowData1[5] = othAssData[k].daysDiff;
-					newSheet.addRow(rowData1);
+					if (otFlag == false) {
+						rowData1[1] = iRData[i].csi;
+						rowData1[2] = iRData[i].fromDate;
+						rowData1[4] = iRData[i].toName;
+						rowData1[3] = othAssData[k].otherDate;
+						rowData1[5] = othAssData[k].daysDiff;
+						otFlag = true;
+						newSheet.addRow(rowData1);
+						debugger
+					}
+
+					if (othAssData.length > 1) {
+						let otLen = othAssData.length;
+
+						rowData5[1] = othAssData[k].csi;
+						rowData5[2] = othAssData[k].otherDate;
+						rowData5[4] = othAssData[k].otherName;
+						if (otLen != (k + 1)) {
+							rowData5[3] = othAssData[k + 1].otherDate;
+							rowData5[5] = othAssData[k + 1].daysDiff;
+						}
+						otherAsDataAdd.push({ csi: rowData5[1], days: rowData5[5] });
+						newSheet.addRow(rowData5);
+
+					}
 				}
 			}
 		} else if (iRData.length > 0 && iPrData.length > 0) {
@@ -285,6 +314,7 @@ function writeExcel() {
 					rowiPData1[3] = iPrData[i].toDate;
 					rowiPData1[5] = iPrData[i].totalDays;
 					newSheet.addRow(rowiPData1);
+
 				}
 			}
 		} else {
@@ -294,47 +324,54 @@ function writeExcel() {
 			rowiRData1[3] = "";
 			rowiRData1[5] = iRData[i].tillTotalDays;
 			newSheet.addRow(rowiRData1);
+			debugger
 		}
 
 		if (iPrData.length > 0) {
-
 			for (let j = 0; j < iPrData.length; j++) {
-				let iprLength = iPrData.length;
-
 				if (iRData[i].csi == iPrData[j].csi) {
-					debugger
+
 					if (rowData2.length == 0) {
 						rowData2[1] = iPrData[j].csi;
-						rowData2[2] = iRData[i].fromDate;
 						rowData2[3] = iPrData[j].toDate;
+
 						if (othAssData.length > 0) {
+							let otLen = othAssData.length;
 							for (let k = 0; k < othAssData.length; k++) {
+
+								rowData2[2] = othAssData[otLen - 1].otherDate;
 								rowData2[4] = othAssData[k].otherName;
+								let startDT = rowData2[3];
+								let endDT = rowData2[2];
+								var startDate = Date.parse(startDT);
+								var endDate = Date.parse(endDT);
+								var timeDiff = startDate - endDate;
+								var daysDiffOt = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+								rowData2[5] = daysDiffOt;
 								othAssData = [];
 							}
 						} else {
+							rowData2[2] = iRData[i].fromDate;
+							rowData2[3] = iPrData[j].toDate;
 							rowData2[4] = iPrData[j].toName;
+							rowData2[5] = iPrData[j].totalDays;
 						}
-						rowData2[5] = iPrData[j].totalDays;
 						newSheet.addRow(rowData2);
+
 					} else {
 						break;
 					}
-
-					;
 				}
 			}
 		}
 		if (othAssData.length > 0) {
-
 			for (let k = i; k < othAssData.length; k++) {
-				// if (othAssData[k] != null) {
-				debugger
+
 				if (iRData[i].csi == othAssData[k].csi) {
 					rowData3[1] = othAssData[k].csi;
-					rowData3[2] = iRData[k].fromDate;
+					rowData3[2] = iRData[i].fromDate;
 					if (iPrData.length > 0) {
-
 						for (let j = 0; j < iPrData.length; j++) {
 							rowData3[3] = iPrData[j].toDate;
 						}
@@ -350,12 +387,12 @@ function writeExcel() {
 					newSheet.addRow(rowData3);
 
 				}
-				// }
 			}
 		}
 	}
 
-	if (rowData1 || rowData2 || rowData3) {
+	if (rowData1.length > 0 || rowData2.length > 0 || rowData3.length > 0) {
+
 		totalData(rowData1, rowData2, rowData3);
 	}
 }
@@ -367,8 +404,10 @@ function totalData(rowData1, rowData2, rowData3) {
 	rowData4[2] = "";
 	rowData4[3] = "";
 	rowData4[4] = "Total";
-	debugger
+
 	if (rowData1.length > 0 && rowData2.length > 0 && rowData3.length > 0 && flag2 == false && flag3 == false) {
+		flag1 = true;
+		debugger
 		if (rowiPData1.length > 0) {
 			if (rowiPData1[1] == rowData1[1]) {
 				if ((rowData1[1] == rowData2[1])) {
@@ -376,19 +415,15 @@ function totalData(rowData1, rowData2, rowData3) {
 					let scnd = rowiPData1[5];
 					let third = rowData2[5];
 					rowData4[5] = fst + scnd + third;
-					flag1 = true;
 				}
 				if ((rowData1[1] == rowData3[1])) {
 					let fst = rowData1[5];
 					let scnd = rowiPData1[5];
 					let third = rowData3[5];
 					rowData4[5] = fst + scnd + third;
-					flag1 = true;
 				}
 			} else {
-				debugger
 				rowData4[5] = rowiPData1[5];
-				flag1 = true;
 			}
 		}
 		if (rowiRData1.length > 0) {
@@ -398,70 +433,90 @@ function totalData(rowData1, rowData2, rowData3) {
 					let scnd = rowiRData1[5];
 					let third = rowData2[5];
 					rowData4[5] = fst + scnd + third;
-					flag1 = true;
 				}
 				if ((rowData1[1] == rowData3[1])) {
 					let fst = rowData1[5];
 					let scnd = rowiRData1[5];
 					let third = rowData3[5];
 					rowData4[5] = fst + scnd + third;
-					flag1 = true;
 				}
 			} else {
 				rowData4[5] = rowiRData1[5];
-				flag1 = true;
+
 			}
 			if (rowData1[1] == rowiRData1[1]) {
 				let fst = rowData1[5];
 				let scnd = rowData2[5];
 				let third = rowData3[5];
 				rowData4[5] = fst + scnd + third;
-				flag1 = true;
+
 			}
 		}
+
 	}
 
 	if (rowData1.length > 0 && rowData2.length > 0 && flag1 == false && flag3 == false) {
+		debugger
+		flag2 = true;
 		if (rowData1[1] == rowData2[1]) {
 			let fst = rowData1[5];
 			let scnd = rowData2[5];
 			rowData4[5] = fst + scnd;
-			flag2 = true;
 		}
 
+		if (otherAsDataAdd.length > 0) {
+			let fstArraySum = 0;
+			for (let i = 0; i < otherAsDataAdd.length; i++) {
+				if (otherAsDataAdd[i].csi == rowData2[1]) {
+					fstArraySum += otherAsDataAdd[i].days;
+					let fst = fstArraySum;
+					let scnd = rowData2[5];
+					let third = rowData1[5];
+					rowData4[5] = fst + scnd + third;
+				}
+			}
+
+		}
+		if (rowiRData1.length > 0) {
+			rowData4[5] = rowiRData1[5];
+		}
 	}
 	if (rowData1.length > 0 && rowData3.length > 0 && flag1 == false && flag2 == false) {
+		flag3 = true;
 		if (rowData1[1] == rowData3[1]) {
 			let fst = rowData1[5];
 			let scnd = rowData3[5];
 			rowData4[5] = fst + scnd;
-			flag3 = true;
 		}
 	}
-	debugger
+
 	newSheet.addRow(rowData4);
 
+
 	if (flag1 == true) {
-		debugger
 		rowData1 = [];
 		rowData2 = [];
 		rowData3 = [];
 		rowiRData1 = [];
 		rowiPData1 = [];
+		rowData5 = [];
+		rowData4 = [];
 	} else if (flag2 == true) {
-		debugger
 		rowData1 = [];
 		rowData2 = [];
 		rowData3 = [];
 		rowiRData1 = [];
 		rowiPData1 = [];
+		rowData5 = [];
+		rowData4 = [];
 	} else {
-		debugger
 		rowData1 = [];
 		rowData2 = [];
 		rowData3 = [];
 		rowiRData1 = [];
 		rowiPData1 = [];
+		rowData5 = [];
+		rowData4 = [];
 	}
 }
 
